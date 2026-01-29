f3467e989f54021e2f5c8edc2e5f84de2f94b300
Kajaqq <kajos200@gmail.com>
2026-01-29 11:27:59 +0100

    Update main.py with new sources

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+import italy_parser
 import spain_parser
 import france_gov_parser
+import france_js_parse
 import camera_check
 import asyncio
 from utils import CONSTANTS
 
-
 SEP = CONSTANTS.COMMON.SEPARATOR
+
 
 def check_cameras(data, download=True):
     # Set 'download' to False to not verify based on image similarity
     asyncio.run(camera_check.main(camera_json=data, download=download))
+
 
 def get_spain_data(output_file=None):
     print(SEP)
@@ -24,11 +27,28 @@ def get_france_data(output_file=None):
     print(SEP)
     return france_gov_parser.get_parsed_data(output_file)
 
+
+def get_france_js_data(output_file=None):
+    print(SEP)
+    print("Downloading France JS data...")
+    print(SEP)
+    return france_js_parse.get_parsed_data(output_file)
+
+
+def get_italy_data(output_file=None):
+    print(SEP)
+    print("Downloading Italy data...")
+    print(SEP)
+    return italy_parser.get_parsed_data(output_file)
+
+
 def main():
     # Set the output file variable below to save the pre-parsed data too
     check_cameras(data=get_spain_data())
     check_cameras(data=get_france_data())
+    check_cameras(data=get_france_js_data())
+    check_cameras(data=get_italy_data())
+
 
 if __name__ == "__main__":
     main()
-
